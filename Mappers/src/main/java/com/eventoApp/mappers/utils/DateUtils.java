2c748e1af5a33d4760d47276a51e30888e62e5ac
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2021-03-10 19:30:58 -0300

    Adding new utils

--- Mappers/src/main/java/com/eventoApp/mappers/utils/DateUtils.java
+++ Mappers/src/main/java/com/eventoApp/mappers/utils/DateUtils.java
@@ -11,7 +11,9 @@ public class DateUtils {
     // - MM:   month in year (number)
     // - yyyy: year
 	
-    private static SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    private static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private static SimpleDateFormat hourFormat = new SimpleDateFormat("HH:mm");
     
     // read a date string and parse/convert to a date
     public static Date parseDate(String dateStr) throws ParseException {
@@ -20,10 +22,11 @@ public class DateUtils {
         return theDate;        
     }
     
+    
     // read a date and format/convert to a string
     public static String formatDate(Date theDate) {
         
-        String result = null;
+        String result = "";
         
         if (theDate != null) {
             result = formatter.format(theDate);
@@ -32,4 +35,18 @@ public class DateUtils {
         return result;
     }
     
+    
+    public static String getDate(String dateStr) throws ParseException {
+
+    	Date date = parseDate(dateStr);
+    	return dateFormat.format(date);    	
+    }
+    
+    
+    public static String getHour(String dateStr) throws ParseException {
+
+    	Date date = parseDate(dateStr);
+    	return hourFormat.format(date);    	
+    }
+ 
 }
